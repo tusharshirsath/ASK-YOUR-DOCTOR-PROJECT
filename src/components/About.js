975066b83fe4c4ac619f3ce45ebf75e88e590e83
Tushar Shirsath <105260643+tusharshirsath@users.noreply.github.com>
2026-05-16 09:46:24 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                     Ask Your Doctor is a platform for booking medical consultations with specialist doctors in your city online. Patient can book an
                     appointment by selecting any of the time slot given by doctor.</p>
                 <p> Some additional functionalities are - user can search
-                    doctor by specialization and city, both user and doctor can manage their booked appointments, both can manage
+                    doctor by specialization,Experience and city, both user and doctor can manage their booked appointments, both can manage
                     their appointment history, verification of doctor's credentials by admin and users can get information about blood donors in their city.
                 </p>
                 <p className="text-muted">Regards, from creators:
